7ac66048e170bbeaaf76ff40a16a4fddf44f8fc0
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-14 18:57:19 +0100

    Add project files.

---
+++ Ch05Ex06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ch05Ex06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "This is a test";
+            char[] seperator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(seperator);
+
+            foreach(string word in myWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
